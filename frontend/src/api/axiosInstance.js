6f5f4b44624d8f18fe649db092d482a3f4ea0be8
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-01-19 20:58:08 +0300

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,12 +3,13 @@ import { BASE_URL } from './routes';
 import { setupInterceptors } from './interceptors';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5173/',
+  baseURL: window.location.origin,
   headers: { 'Content-Type': 'application/json' },
 });
 
 export const apiWithIterceptors = setupInterceptors(axiosInstance);
 
 export default apiWithIterceptors;
+
 
 
